16972b1df64f0ab16314a924f3d355f7e4424a89
Luiz Gustavo <102548854+LuizGustavoHeros@users.noreply.github.com>
2023-02-17 20:17:17 +0000

    atualizando o botão, apenas mobile

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function clicou() {
   const botaoFechar = document.getElementById('botao-close')
   const botaoAbrir = document.getElementById('botao-open')
-
+  const nav = document.getElementById('nav')
+  const main = document.querySelector('main')
+  const footer = document.querySelector('footer')
 
   if(botaoFechar.innerHTML === `<img src="./assets/menu-buguer-close.svg">` ) {
     botaoFechar.innerHTML = ''
@@ -10,4 +12,7 @@ function clicou() {
   }
   
   botaoAbrir.classList.toggle('abrir')
+  nav.classList.toggle('navegacao-open')
+  main.classList.toggle('abrir')
+  footer.classList.toggle('abrir')
 }
